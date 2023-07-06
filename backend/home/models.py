55dd4e14db019032af64b424419969f281e1b23a
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-06 14:11:55 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+class Pet(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=200,)
+    type = models.CharField(max_length=200,)
+    image_url = models.CharField(max_length=200,)
+    attributes = models.TextField()
+    is_favorite = models.BooleanField(default="False",)
+    description = models.TextField()
+    date_adopted = models.DateTimeField(auto_now=True,)
